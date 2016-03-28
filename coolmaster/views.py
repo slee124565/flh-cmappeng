3e07d3bdb5ec19cbbc03dfe72e08f696b625a2c3
Lee Shiueh <lee.shiueh@gmail.com>
2016-03-28 14:34:22 +0800

    implement bytecmd_api function

--- coolmaster/views.py
+++ coolmaster/views.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponse
 from serial import SerialException
 import serial, logging, json, time
+import binascii
 
 from coolmaster.models import get_coolmaster_instrument
 
@@ -9,7 +10,80 @@ logger = logging.getLogger(__name__)
 # Create your views here.
 
 def bytecmd_api(request):
-    pass
+    """
+    Trigger CoolMaster to write byte array to RS232 serial port. 
+    
+    Handle HTTP Get request with hex string argument from network 
+    and trigger CoolMaster to covert hex string to byte array and
+    write to RS232 serial port.
+    The HTTP GET URL example:: 
+        http://url_request_path/?arg=1a2c3d4b
+    
+    Parameters:
+    
+    - `arg`: the CoolMaster command argument value in querystring. 
+    """
+    hexstring = request.GET.get('arg')
+    logger.info('http request coolmaster execute byte command with arg $s' % hexstring)
+    bdata = binascii.unhexlify(hexstring)
+
+    try:
+        instr = get_coolmaster_instrument()
+        ncount = 1
+        while ncount < 5:
+            if instr.isOpen() == False:
+                logger.debug('opening serial port %s' % instr.port)
+                try:
+                    instr.open()
+                except SerialException:
+                    logger.warning('serial port is already in used.')
+                    rtn_data = b'ERROR: Serial Port In Used'
+                    break
+            nbyte = instr.write(bdata)
+            instr.flush()
+            logger.debug('byte command write')
+            time.sleep(1)
+            
+            rtn_data = b''
+            lines = instr.readlines()
+            logger.debug('readlines count %d' % len(lines))
+
+            logger.debug('is data remain %d' % instr.inWaiting())
+
+            for line in lines:
+                rtn_data += line
+            logger.debug('get response data : \n%s' % rtn_data.decode())
+
+            if len(rtn_data) == 0:
+                logger.warning('no data received from serial connection!')
+                instr.close()
+                time.sleep(0.5)
+                logger.warning('retry %d time' % ncount)
+                ncount += 1
+            else:
+                break
+
+        if len(rtn_data) == 0:
+            logger.error('no response data for byte command')
+            resp_data = 'ERROR: No Data from Serial Port'
+        else:
+            rtn_hex = ''
+            for i in range(len(rtn_data)):
+                rtn_hex += hex(rtn_data[i]) + ' '
+            logger.debug('rtn data hex: %s' % rtn_hex)
+            resp_data = 'byte command execute: ' + hexstring + '\n' + rtn_data.decode().strip()
+    except:
+        logger.error('Serial Port Exception!', exc_info=True)
+        resp_data = 'CMStation Internal ERROR'
+            
+        instr.close()
+    
+    response = HttpResponse(content_type='text/plain')
+    
+
+    response.content = resp_data
+    return response
+
 
 def cm_api(request,cmd):
     """
