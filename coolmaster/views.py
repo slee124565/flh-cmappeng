11692b273333b3652d20816ac5c8b40092b54404
Lee Shiueh <lee.shiueh@gmail.com>
2016-05-23 00:08:01 +0800

    refactore cm_api function

--- coolmaster/views.py
+++ coolmaster/views.py
@@ -102,70 +102,64 @@ def cm_api(request,cmd):
     else:
         cmd_exec = cmd + ' ' + arg + '\r\n'
 
+
     try:
+
         instr = get_coolmaster_instrument()
-        ncount = 1
-        while ncount < 5:
-            if instr.isOpen() == False:
-                logger.debug('opening serial port %s' % instr.port)
-                try:
-                    instr.open()
-                except SerialException:
-                    logger.warning('serial port is already in used.')
-                    rtn_data = b'ERROR: Serial Port In Used'
-                    break
-            nbyte = instr.write(cmd_exec.encode())
-            instr.flush()
-            logger.debug('write command [%s] and %d bytes sent.' 
-                                % (cmd_exec.replace('\r\n','\\r\\n'),nbyte))
-            time.sleep(1)
-            
-            rtn_data = b''
+        instr.open()
+        nbyte = instr.write(cmd_exec.encode())
+        logger.debug('write command [%s] and %d bytes sent.' 
+                            % (cmd_exec.replace('\r\n','\\r\\n'),nbyte))
+        
+        if cmd == 'stat':
+            logger.debug('enter stat check process')
             lines = instr.readlines()
             logger.debug('readlines count %d' % len(lines))
-
-            logger.debug('is data remain %d' % instr.inWaiting())
-
+            rtn_data = b''
             for line in lines:
                 rtn_data += line
             logger.debug('get response data : \n%s' % rtn_data.decode())
-
+            
             if len(rtn_data) == 0:
-                logger.warning('no data received from serial connection!')
-                instr.close()
-                time.sleep(0.5)
-                logger.warning('retry %d time' % ncount)
-                ncount += 1
+                logger.error('no response data for command [%s]' % cmd_exec.replace('\r\n','\\r\\n'))
+                resp_data = 'ERROR: No Data from Serial Port'
             else:
-                break
-
-        if len(rtn_data) == 0:
-            logger.error('no response data for command [%s]' % cmd_exec.replace('\r\n','\\r\\n'))
-            resp_data = 'ERROR: No Data from Serial Port'
+                if len(rtn_data) > 0 and rtn_data[0] == 0:
+                    logger.warning('receive byte array exist 0x00 at the begining, remove head 0x00 byte')
+                    rtn_data = rtn_data[1:]
+                if len(rtn_data) > 0 and rtn_data[-1] == 0:
+                    logger.warning('reveive byte array exist 0x00 at the end, removed endj 0x00 byte')
+                    rtn_data = rtn_data[:-1]
+    
+                rtn_hex = ''
+                for i in range(len(rtn_data)):
+                    rtn_hex += hex(rtn_data[i]) + ' '
+                logger.debug('rtn data hex: %s' % rtn_hex)
+                resp_data = rtn_data.decode().strip()
+                logger.debug('%s' % resp_data)
+                
+                #-> return last stat result
+                pos = resp_data.rfind('stat')
+                if pos > 0:
+                    resp_data = resp_data[pos:]
+                    logger.debug('stat result return:\n%s' % resp_data)
+                    
         else:
-            if len(rtn_data) > 0 and rtn_data[0] == 0:
-                logger.warning('receive byte array exist 0x00 at the begining, remove head 0x00 byte')
-                rtn_data = rtn_data[1:]
-            if len(rtn_data) > 0 and rtn_data[-1] == 0:
-                logger.warning('reveive byte array exist 0x00 at the end, removed endj 0x00 byte')
-                rtn_data = rtn_data[:-1]
-
-            rtn_hex = ''
-            for i in range(len(rtn_data)):
-                rtn_hex += hex(rtn_data[i]) + ' '
-            logger.debug('rtn data hex: %s' % rtn_hex)
-            resp_data = 'command execute: ' + cmd_exec + '\n' + rtn_data.decode().strip()
+            if nbyte > 0:
+                resp_data = 'Command Write OK'
+            else:
+                resp_data = 'Command Write Fail'
+                logger.error('command [%s] write fail' % cmd_exec.replace('\r\n','\\r\\n'))
+                
     except:
         logger.error('Serial Port Exception!', exc_info=True)
         resp_data = 'CMStation Internal ERROR'
-            
+    
+    finally:        
         instr.close()
-    
-    response = HttpResponse(content_type='text/plain')
-    
-
-    response.content = resp_data
-    return response
+        response = HttpResponse(content_type='text/plain')
+        response.content = resp_data
+        return response
 
 def hc2_vd_update_simulation(request):
     logger.info('hc2_vd_update_simulation triggered')
